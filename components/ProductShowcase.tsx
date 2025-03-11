6c4de18a908c23c7a27c5b006a901681050b7d0d
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-03-11 08:33:58 +0700

    fixed: CardMoonCake component

--- components/ProductShowcase.tsx
+++ components/ProductShowcase.tsx
@@ -15,12 +15,14 @@ const ProductShowcase = () => {
         />
         <CardMoonCake
           discountLabel="/images/top-choice.png"
+          soldOut="/images/soldout.png"
           title="BTT Đậu Xanh Hoàng Kim"
           price={105000}
           firstImage="/images/banh-trung-thu-hoi-an-mooncake-8.jpg"
           secondImage="/images/nhan-banh-trung-thu-hoianmooncake-4.jpg"
         />
         <CardMoonCake
+          discountLabel="/images/limited.png"
           soldOut="/images/soldout.png"
           title="BTT Hạt Sen – Hạt Chia – Mắc Ca (1 Trứng)"
           price={135000}
@@ -28,6 +30,7 @@ const ProductShowcase = () => {
           secondImage="/images/nhan-banh-trung-thu-hoianmooncake-6.jpg"
         />
         <CardMoonCake
+          discountLabel="/images/limited.png"
           soldOut="/images/soldout.png"
           title="BTT Khoai Môn – Hạt Macca (1 Trứng)"
           price={145000}
@@ -51,6 +54,7 @@ const ProductShowcase = () => {
           secondImage="/images/nhan-banh-trung-thu-hoianmooncake-8.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
           discountLabel="/images/limited.png"
           title="BTT Hải Sâm Tôm Hùm (1 Trứng)"
           price={165000}
@@ -58,48 +62,64 @@ const ProductShowcase = () => {
           secondImage="/images/nhan-banh-trung-thu-hoianmooncake-5.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Bào Ngư Sốt Rượu Vang (1 Trứng)"
           price={165000}
           firstImage="/images/banh-trung-thu-hoi-an-mooncake-4.jpg"
           secondImage="/images/nhan-banh-trung-thu-hoianmooncake-7.jpg"
         />
         <CardMoonCake
+          discountLabel="/images/top-choice.png"
+          soldOut="/images/soldout.png"
           title="BTT Thập Cẩm Cá Hồi Sốt Chanh Leo (1 trứng)"
           price={165000}
           firstImage="/images/banh-trung-thu-hoi-an-mooncake-4.jpg"
           secondImage="/images/banh-trung-thu-hoi-an-mooncake-4.jpg"
         />
         <CardMoonCake
+          discountLabel="/images/top-choice.png"
+          soldOut="/images/soldout.png"
           title="BTT Yến Sào Hạt Sen Trà Xanh – Macca"
           price={155000}
           firstImage="/images/banh-trung-thu-buu-yen-2.jpg"
           secondImage="/images/banh-trung-thu-buu-yen-2.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Yến Sào Hạt Sen – Hạnh Nhân – Saffron"
           price={155000}
           firstImage="/images/banh-trung-thu-buu-yen-3.jpg"
           secondImage="/images/banh-trung-thu-buu-yen-3.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Nhất Yến Thượng Hạng"
           price={165000}
           firstImage="/images/banh-trung-thu-buu-yen-4.jpg"
           secondImage="/images/banh-trung-thu-buu-yen-4.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Yến Sào Hạt Sen – Đông Trùng Hạ Thảo"
           price={165000}
           firstImage="/images/banh-trung-thu-buu-yen-5.jpg"
           secondImage="/images/banh-trung-thu-buu-yen-5.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Yến Sào Bào Ngư – Vi Cá (2 Trứng)"
           price={195000}
           firstImage="/images/banh-trung-thu-buu-yen-6.jpg"
           secondImage="/images/banh-trung-thu-buu-yen-6.jpg"
         />
         <CardMoonCake
+          soldOut="/images/soldout.png"
+          discountLabel="/images/limited.png"
           title="BTT Yến Sào Hải Sâm – Sò Điệp (2 Trứng)"
           price={195000}
           firstImage="/images/banh-trung-thu-buu-yen-1.jpg"
